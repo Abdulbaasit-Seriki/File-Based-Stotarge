1901b9722b2a59bf348403a64b664aa82cb8eb8d
Abdulbaasit <baasisek01@gmail.com>
2020-05-06 11:38:42 +0100

    Added creating user functionality

--- Repos/users.js
+++ Repos/users.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 class Users {
+
 	constructor(filename) {
 
 		if (!filename) {
@@ -18,15 +19,25 @@ class Users {
 		}
 	}
 
-	async getFileContents ()  {
+	async getAllUsers ()  {
 		return JSON.parse(await fs.promises.readFile(this.filename, {
 			encoding: 'utf8'
 		}));
+	},
+
+	async createUser (options) {
+		// Get the contents of the file
+		const fileContent = await this.getAllUsers;
+		// Add the new data to it
+		fileContent.push(options);
+
+		await fs.promises.writeFile(this.filename, JSON.stringify(fileContent));
 	}
 }
 
 const test = async () => {
 	const newUser = new Users('users.json');
-	console.log(await newUser.getFileContents());
+	await newUser.createUser({name: hey, email: test@gmail.com});
+	console.log(await newUser.getAllUsers());
 }
 test();
