125b56832345c25c3fc0fe36e8b521e784bf9789
Abdulbaasit <baasisek01@gmail.com>
2020-06-22 16:55:01 +0100

    feat: added edit functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,23 +3,24 @@ const multer = require('multer');
 
 const productsRepo = require('../../Repos/products');
 const { validateProductTitle, validateProductPrice } = require('./validators');
-const { handleValidationErrors } = require('./middlewares');
+const { handleValidationErrors, requireAuth } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 let errors;
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
 	const products = await productsRepo.getAll();
 	res.render('admin/products/index', { products });
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.render('admin/products/new', {errors: null});
 });
 
 router.post(
   '/admin/products/new',
+  requireAuth,
   upload.single('image'),
   [validateProductTitle, validateProductPrice],
   handleValidationErrors('admin/products/new'),
@@ -32,5 +33,14 @@ router.post(
     res.redirect('/admin/products');
   }
 );
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+	const product = await productsRepo.getById(req.params.id);
+
+	if (!product) {
+		return res.send(`Product not found`);
+	}
+	res.render('admin/products/edit', { product });
+})
 
 module.exports = router;
